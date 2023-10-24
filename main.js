4603707c240edd2cb59b768d0bab170737a8d540
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-10-24 06:11:17 -0600

    Starting off, got some help with the starting javascript

--- main.js
+++ main.js
@@ -1,46 +1,63 @@
-async function getAvailableCourses() {
-  const url =
-    "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json";
-  const response = await fetch(url);
-  const course = await response.json();
-    console.log(course);
-  return course;
-}
-function getGolfCourseDetails(golfCourseId) {
+var courses;
+var teeBoxes;
+function getAvailableGolfCourses() {
   return fetch(
-    `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`,
-    { mode: "no-cors" }
+    "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json"
   ).then(function (response) {
-    console.log(response.json)
     return response.json();
   });
 }
-getAvailableCourses();
-getGolfCourseDetails();
-let courseOptionsHtml = "";
-courses.forEach((course) => {
-  courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
-});
 
-document.getElementById("course-select").innerHTML = courseOptionsHtml;
-
-let teeBoxSelectHtml = "";
-teeBoxes.forEach(function (teeBox, index) {
-  teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
-    teeBox.totalYards
-  } yards</option>`;
-});
-document.getElementById("tee-box-select").innerHTML = teeBoxSelectHtml;
-
-class Player {
-  constructor(name, id = getNextId(), scores = []) {
-    this.name = name;
-    this.id = id;
-    this.scores = scores;
-  }
+function playersName(){
+    let numberOfPlayers = document.getElementById("playerCount").value;
+    let nameInput = '';
+    console.log(numberOfPlayers);
+    if(numberOfPlayers == 1){
+        console.log("one player");
+    }
 }
-function getNextId() {
-  const uniqueId = Math.random().toString(36).substr(2, 9);
-  console.log(uniqueId);
-  return uniqueId;
+function getGolfCourseDetails(golfCourseId) {
+  return fetch(
+    `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`
+  ).then(function (response) {
+    return response.json();
+  });
 }
+
+function load() {
+  getAvailableGolfCourses().then((result) => {
+    courses = result;
+
+    
+    console.log(courses);
+    let courseOptionsHtml = "";
+    courses.forEach((course) => {
+      courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+    });
+    document.getElementById("course-select").innerHTML = courseOptionsHtml;
+    golfCourseId = courses.id;
+  });
+  const courseSelect = document.getElementById("course-select");
+  courseSelect.addEventListener("change", function () {
+    const selectedCourseId = courseSelect.value;
+
+    getGolfCourseDetails(selectedCourseId).then((courseDetails) => {
+      let hole = courseDetails.holes;
+      teeBoxes = hole[0].teeBoxes;
+      console.log(teeBoxes)
+      let teeBoxSelectHtml = "";
+      teeBoxes.forEach((teeBox, index) => {
+        teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+          teeBox.yards
+        } yards</option>`;
+      });
+      document.getElementById("tee-box-select").innerHTML = teeBoxSelectHtml;
+      
+    });
+   
+  });
+}
+load();
+
+//TODO: local sotage
+
