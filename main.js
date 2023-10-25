49f1635654240f676b6f27c05ddbc0b914a4ecbe
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-10-25 07:50:53 -0600

    New Player and scoreboard rendering start.

--- main.js
+++ main.js
@@ -1,63 +1,129 @@
+// Global variables
 var courses;
 var teeBoxes;
+var golfCourseId;
+
+// Function to get available golf courses
 function getAvailableGolfCourses() {
-  return fetch(
-    "https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json"
-  ).then(function (response) {
-    return response.json();
-  });
+  return fetch("https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/courses.json")
+    .then(function (response) {
+      return response.json();
+    });
 }
 
-function playersName(){
-    let numberOfPlayers = document.getElementById("playerCount").value;
-    let nameInput = '';
-    console.log(numberOfPlayers);
-    if(numberOfPlayers == 1){
-        console.log("one player");
+// Class definition for Player
+class Player {
+  constructor(name, id, scores = []) {
+    this.name = name;
+    this.id = id;
+    this.scores = scores;
+  }
+}
+
+// Function to add a new player
+function addPlayer() {
+  let nameInput = document.createElement("input");
+  nameInput.id = "playerName";
+  nameInput.type = "text";
+  nameInput.className = "w-25";
+  nameInput.placeholder = "your name";
+  document.getElementById("nameInputHere").appendChild(nameInput);
+
+  nameInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      const uniqueId = new Date().getTime().toString();
+      let playerName = nameInput.value;
+      console.log(playerName);
+      document.getElementById("nameInputHere").innerHTML = " ";
+
+      var newPlayer = new Player(playerName, uniqueId);
+      console.log(newPlayer);
+
+      let nextPlayer = "";
+      nextPlayer += `<tr> <th colspan="2" id="${uniqueId}">${playerName}</th> <td> 0<td> </tr>`;
+      document.getElementById("scorecardBody").innerHTML += nextPlayer;
+
+      document.getElementById("scorecardBodyBack").innerHTML += nextPlayer;
     }
-}
-function getGolfCourseDetails(golfCourseId) {
-  return fetch(
-    `https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`
-  ).then(function (response) {
-    return response.json();
   });
 }
 
+// Function to get golf course details
+function getGolfCourseDetails(golfCourseId) {
+  return fetch(`https://exquisite-pastelito-9d4dd1.netlify.app/golfapi/course${golfCourseId}.json`)
+    .then(function (response) {
+      return response.json();
+    });
+}
+
+// Function to load golf course data and populate the UI
 function load() {
   getAvailableGolfCourses().then((result) => {
     courses = result;
-
-    
-    console.log(courses);
     let courseOptionsHtml = "";
+
     courses.forEach((course) => {
       courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
     });
+
     document.getElementById("course-select").innerHTML = courseOptionsHtml;
     golfCourseId = courses.id;
   });
+
   const courseSelect = document.getElementById("course-select");
   courseSelect.addEventListener("change", function () {
     const selectedCourseId = courseSelect.value;
 
     getGolfCourseDetails(selectedCourseId).then((courseDetails) => {
+      // Show Holes on Scorecard
       let hole = courseDetails.holes;
+      let holes = "";
+      console.log(hole);
+      for (let i = 0; i < 9; i++) {
+        if (hole[i]) {
+          holes += `<th>${hole[i].hole}</th>`; // Use hole[i] to access the specific hole
+        }
+      }
+      let secondHoles = "";
+      let i = 7;
+      while( i > 8, i < 17){
+        i++;
+        console.log("test")
+          secondHoles += `<th>${hole[i].hole}</th>`
+      }
+      for (let i = 0; i > 8; i ++) {
+        if(hole[i]){
+          console.log("test")
+          secondHoles += `<th colspan="1">${hole[i].hole}</th>`
+        }
+      }
+
+      document.getElementById("firstNumbers").innerHTML = `<th colspan="2">Hole</th>` + holes + `<th> Out </th>`;
+      document.getElementById("secondNumbers").innerHTML = `<th colspan="2"> Hole </th>` + secondHoles + `<th> Total </th>`
+
+      // Teeboxes input
       teeBoxes = hole[0].teeBoxes;
-      console.log(teeBoxes)
+      console.log(teeBoxes);
+
       let teeBoxSelectHtml = "";
+
       teeBoxes.forEach((teeBox, index) => {
-        teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
-          teeBox.yards
-        } yards</option>`;
+        teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}</option>`;
       });
+
+      let teeboxSelections = document.getElementById("tee-box-select");
       document.getElementById("tee-box-select").innerHTML = teeBoxSelectHtml;
-      
+
+      // Yardage and Par for each on scoreboard
+      teeboxSelections.addEventListener("change", (e) => {
+        console.log("selctions " + teeboxSelections.value);
+      });
     });
-   
   });
 }
+
+// Initial function call
 load();
 
-//TODO: local sotage
-
+// TODO: local storage
+// 391 yards American Fork index=0, 521 index=1
